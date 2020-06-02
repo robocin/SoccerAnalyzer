90e43ef3de78c832b2f42c006eb1080b91041cf9
mateusfbsoares <mateusfbsoares@gmail.com>
2020-06-02 20:05:54 -0300

    fixed conflict

--- source/playerClass.py
+++ source/playerClass.py
@@ -2,7 +2,6 @@ import numpy as numpy
 import pandas as pd
 
 import positionClass
-<<<<<<< HEAD
 import computingFunctions as computing
 import fault
 
@@ -26,25 +25,6 @@ class Player():
 
         #calls for computing
         self.compute()
-=======
-import eventClass
-
-class Player:
-    def __init__(self, team = "No team", number = 0, pos = "No position", f_pro = 0, f_commited = 0,
-    f_shot = 0, goals = 0, tries = 0, good_try = 0, tackles = 0):
-
-        self.__team = team
-        self.__number = number
-        self.__pos = pos
-        self.__f_pro = f_pro
-        self.__f_commited = f_commited
-        self.__f_shot = f_shot
-        self.__goals = goals
-        self.__tries = tries
-        self.__good_try = good_try
-        self.__tackles = tackles
-
->>>>>>> cd0cd2c394fa63d059cd0946dee4b55c79d286e2
 
     #set methods
    
