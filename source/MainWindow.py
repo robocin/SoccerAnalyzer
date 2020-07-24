ff150ce1dc9550bc181bc18cd828033dd8e78839
mateusfbsoares <mateusfbsoares@gmail.com>
2020-07-24 14:48:31 -0300

    fixed indetation bug

--- source/MainWindow.py
+++ source/MainWindow.py
@@ -202,35 +202,6 @@ class MainWindow(QMainWindow):
         elif(graph_type == "Quantidade relativa de gols"):
             self.game_info.plot_goals_percentage() 
 
-            # COMENTADO A SEGUIR, TEST PIE e TEST BAR deverão ser excluídos, inclusive da lista, estão aqui apenas para servir de referência durante o desenvolvimento.
-       
-"""
-        elif(graph_type == "TEST PIE"):
-            self.plot_Pie("TEST PIE - APENAS PARA REFERÊNCIA ")
-        elif(graph_type == "TEST BAR"):
-            
-            data = PlotData() # COMENTÁRIOS (Felipe)
-                              # não está sendo inicializado, falta parâmetros
-                              # Opção: 1
-                              #     Parametrizar
-                              # Opção: 2
-                              #     Inicializar com parâmetros default na própria classe 
-            
-            data.appendBars(1,["test bar"]) # Este método não está definido no PlotData
-            
-            data.set_x_label("x label")
-            data.set_y_label("y label")
-            
-            bar = data.getBar(0) # ESta método não está definida no PlotData
-            
-            bar.set_name("Bar name")
-            bar.set_value(100) 
-            bar.set_label("Bar label")
-            
-            self.plot_Bar("TEST BAR - APENAS PARA REFERÊNCIA",data)
-        '''(...)'''
-"""
-
         return space
 
     def plot_Bar(self, title, data):
