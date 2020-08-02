437490b1390321e47d35b8a264106a40a108f3b8
mfbs <mateusfbsoares@gmail.com>
2020-08-02 16:26:48 -0300

    heatmap bug fix

--- source/DataCollector.py
+++ source/DataCollector.py
@@ -287,74 +287,7 @@ class DataCollector():
 			table = df.pivot('Y', 'X', 'Value')
 			sb.heatmap(table, ax = axes) 
 			axes.invert_yaxis()
-			print(table)
 
-		# draft from clipboard contents
-		if (graph_type == "_heatmap"):
-			df = pd.read_clipboard()
-			'''
-			clipboard contents:
-			X  Y  Value
-			0  1  .6
-			0  2  .3
-			0  3  .2
-			0  4  .7
-			0  5  .8
-			0  6  .4
-			1  1  .8
-			1  2  .4
-			1  3  .9
-			1  4  .3
-			1  5  .1
-			1  6  .7
-			2  1  .5
-			2  2  .3
-			2  3  .6
-			2  4  .5
-			2  5  .1
-			2  6  .8
-			3  1  .3
-			3  2  .5
-			3  3  .3
-			3  4  .3
-			3  5  .4
-			3  6  .8
-			4  1  .6
-			4  2  .3
-			4  3  .2
-			4  4  .7
-			4  5  .8
-			4  6  .4
-			5  1  .8
-			5  2  .4
-			5  3  .9
-			5  4  .3
-			5  5  .1
-			5  6  .9
-			6  1  .7
-			6  2  .5
-			6  3  .3
-			6  4  .6
-			6  5  .5
-			6  6  .1
-			'''
-			table = df.pivot('Y', 'X', 'Value')
-				#Note about a seaborn and matplotlib difference on plotting functions.seaborn's heatmap automatticaly creates a figure,
-				#canvas and axes in which to plot on, popping up a new window. in order for this not to happen, we must give an axes in which to plot on with the ax argument.
-			sb.heatmap(table, ax = axes) 
-			axes.invert_yaxis()
-			#print(table)
-			'''
-			print(table)'s output(if the table is not in this exat format, there's an ugly error):
-			X    0    1    2    3    4    5    6
-			Y                                   
-			1  0.6  0.8  0.5  0.3  0.6  0.8  0.7
-			2  0.3  0.4  0.3  0.5  0.3  0.4  0.5
-			3  0.2  0.9  0.6  0.3  0.2  0.9  0.3
-			4  0.7  0.3  0.5  0.3  0.7  0.3  0.6
-			5  0.8  0.1  0.1  0.4  0.8  0.1  0.5
-			6  0.4  0.7  0.8  0.8  0.4  0.9  0.1
-			'''
 
 		#TODO: is this necessary?
 		# discards the old graph
