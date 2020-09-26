ca500bc5defbfd7f290c16b7dc33446854a6ce1e
mfbs <mateusfbsoares@gmail.com>
2020-09-26 13:24:14 -0300

    add: code flow diagram to DOC

--- install/autogui.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pag
-import time
-
-pag.hotkey('ctrl','alt','t')
-#pag.typewrite('bash\n')
-time.sleep(5)
-pag.typewrite('source activate analyzer\n', interval=0.1)
-pag.typewrite('./install_2.sh\n')
-pag.typewrite('./install_3.sh\n')
