3e47ba36380b40a9e7d23745d77c72befc55fc68
Felipe Nunes <fnap@cin.ufpe.br>
2021-01-18 12:57:05 -0300

    chore(reader): create reader class

---
+++ common/mla/reader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class Reader:
+    def __init__(self):
+        self._games = games
+
+    @staticmethod
+    def read(self, path):
+        raise NotImplementedError
+    
+        
