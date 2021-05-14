2a47e43ac4de1991fcc5eaa5f59386113c820a0d
Felipe Nunes <fnap@cin.ufpe.br>
2021-05-13 22:24:49 -0300

    docs(ballPossession): add docstring to class

--- common/mla/ball_possession/ballPossession.py
+++ common/mla/ball_possession/ballPossession.py
@@ -4,6 +4,50 @@ from common.basic.game import Game
 from common.operations.measures import distance
 
 class BallPossession:
+    """
+        Used to calculate the simple ball possession of the game.
+
+        Attributes
+        ----------
+            private:
+                leftSideTeamPossession : int
+                    amount of cycles the left team was closest to the ball
+
+                rightSideTeamPossession : int
+                    amount of cycles the right team was closest to the ball
+
+                currentGame : common.basic.game.Game
+                    a Game object of the current game
+
+                BALL_X_COLUMN : int
+                    ball x position in the dataframe
+
+                BALL_Y_COLUMN : int
+                    ball y position in the dataframe
+
+        Methods
+        -------
+            private:
+                filterPlaymode(playmode : str) -> None
+                    filters the currentGame dataframe and returns a filtered copy
+
+                closestPlayerSide(cycle : int,
+                                playerLeftPosition : common.basic.point.Point,
+                                playerRightPosition : common.basic.point.Point,
+                                ballPositionThisCycle : common.basic.point.Point) -> str
+                    returns the closest player of the ball in the cycle passed as argument
+
+                calculate() -> None
+                    populates the leftSideTeamPossession and rightSideTeamPossession
+
+            public:
+                getCurrentGame() -> pandas.Dataframe
+                    returns the dataframe of the current game being analyzed
+
+                get() -> [a,b]
+                    returns the leftSideTeamPossession(a) and rightSideTeamPossession(b) in percentual
+
+    """
     def __init__(self, dataFrame : Game):
         self.__leftSideTeamPossession = 0
         self.__rightSideTeamPossession = 0
