1232e3491ccf53cabfb662507d905dbcdfcdd71e
tta13 <tta@cin.ufpe.br>
2022-04-07 17:15:04 -0300

    docs(writter.py): add documentation to Writer class

--- socceranalyzer/common/io/writter.py
+++ socceranalyzer/common/io/writter.py
@@ -3,13 +3,42 @@ from datetime import datetime
 
 
 class Writter:
+    """
+    A class used to write analysis to a json file.
+
+    Writer()
+
+    Attributes
+    ----------
+            private:
+                dict: dict
+                    python dict used to write the json file
+
+    Methods
+    -------
+            public:
+                write(analysis: string, data: dict)
+                    adds analysis, data to dict key-value pairs
+                complete()
+                    saves the analysis file
+    """
     def __init__(self):
         self.dict = {}
 
-    def write(self, analysis, data):
+    def write(self, analysis: str, data: dict):
+        """
+        Adds analysis, data to dict key-value pairs.
+
+                Parameters:
+                        analysis (str): Analysis name
+                        data (dict): data for the analysis
+        """
         self.dict[analysis] = data
 
-    def complete(self):
+    def complete(self):        
+        """
+        Saves json formatted dict to a txt file.
+        """
         now = datetime.now()
         current_time = now.strftime("%H_%M_%S")
         
