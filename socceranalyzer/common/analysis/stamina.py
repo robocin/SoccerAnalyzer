31aa7a1c78ac69f11445902142eff2a319eee652
José Ronald <jronaldsilva11@gmail.com>
2022-04-29 12:04:00 -0300

    docs: common.analysis.stamina (not finished)

--- socceranalyzer/common/analysis/stamina.py
+++ socceranalyzer/common/analysis/stamina.py
@@ -3,6 +3,32 @@ from socceranalyzer.common.enums.sim2d import SIM2D
 from socceranalyzer.common.chore.mediator import Mediator
 
 class Stamina(AbstractAnalysis):
+    """
+        Provides players' stamina attributes over the course of the entire match.
+
+        Attributes
+        ----------
+            private:
+                dataframe : pandas.Dataframe
+                    match's log to be analyzed
+                category : enum
+                    match's category (2D, VSS or SSL)
+                l_player_stamina: list[float]
+                    a list containing stamina attributes of left team players
+                r_player_stamina: list[float]
+                    a list containing stamina attributes of right team players
+
+        Methods
+        -------
+            private:
+                _analyze() -> None
+                    appends players' stamina to their team list
+
+            public:
+                results() -> (list[float], list[float])
+                    
+            
+    """
     def __init__(self, dataframe, category):
         self.__dataframe = dataframe
         self.__category = category
