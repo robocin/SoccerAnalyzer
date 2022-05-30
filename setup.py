0de108883c5805e43dd3322412844ce85fee1199
fnalmeidap <fnap@cin.ufpe.br>
2022-05-30 14:44:54 -0300

    docs(setup.py): adds project metadata

--- setup.py
+++ setup.py
@@ -2,10 +2,31 @@ from setuptools import setup, find_packages
 
 setup(
     name='socceranalyzer',
-    description='2DSIM, VSS and SSL data analysis package',
-    url='https://github.com/robocin/socceranalyzer',
-    author='Felipe Nunes, Mateus Soares, Bruna Alves',
-    version='1.2.0',
+    version='2.0.0',
+    description='Robocup Soccer data analysis package for SIM2D, SSL and VSS categories',
+    url='https://github.com/robocin/SoccerAnalyzer',
+    author='RoboCIn',
+    author_email="robocin@cin.ufpe.br",
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Jupyter',
+        'Framework :: Matplotlib',
+        'Framework :: Robot Framework :: Library',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords="data science analysis robotics jupyter matplotlib pandas",
+    project_urls={
+        'Documentation': 'TBA',
+        'Source': 'https://github.com/robocin/SoccerAnalyzer',
+        'Tracker': 'https://github.com/robocin/SoccerAnalyzer/issues'
+    },
     packages=[package for package in find_packages() if package.startswith("socceranalyzer")],
-    install_requires=['pandas', 'numpy']
+    install_requires=['pandas', 'numpy', 'matplotlib', 'seaborn'],
+    python_requires='>3.10.*'
 )
