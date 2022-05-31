7e965870bf5b8a2d795b5d8a40c6b35e797f72e6
Tiago Henrique <tiagohrpg@gmail.com>
2022-05-31 15:01:50 -0300

    Refactor: update field parameters

--- socceranalyzer/common/basic/field.py
+++ socceranalyzer/common/basic/field.py
@@ -23,12 +23,19 @@ class Field:
                     defines the penalty area in the field
     """
     def __init__(self, width: float, length: float, center: Point,
-                 small_penalty_area: Rectangle, penalty_area: Rectangle):
+                small_penalty_area_left: Rectangle, 
+                small_penalty_area_right: Rectangle,
+                penalty_area_left: Rectangle,
+                penalty_area_right: Rectangle):
+                
         self.__width = width
         self.__length = length
         self.__center = center
-        self.__small_penalty_area = small_penalty_area
-        self.__penalty_area = penalty_area
+        self.__small_penalty_area_left = small_penalty_area_left
+        self.__small_penalty_area_right = small_penalty_area_right
+        self.__penalty_area_left = penalty_area_left
+        self.__penalty_area_right = penalty_area_right
+
 
 
 class Field2D(Field):
