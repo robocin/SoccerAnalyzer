e927402f9ab941e96205c3268e09d24d6b2a4a39
tta13 <tta@cin.ufpe.br>
2022-06-13 11:09:40 -0300

    docs(passing_accuracy.py): update docstrings

--- socceranalyzer/common/analysis/passing_accuracy.py
+++ socceranalyzer/common/analysis/passing_accuracy.py
@@ -4,18 +4,14 @@ from socceranalyzer.common.analysis.abstract_analysis import AbstractAnalysis
 class PassingAccuracy(AbstractAnalysis):
     """
         Used to calculate the passing accuracy of the game.
-        PassingAccuracy(pandas.DataFrame)
+
+        PassingAccuracy(data_frame: pandas.DataFrame, category: SIM2D | VSS | SSL, passing_evaluator: socceranalyzer.common.evaluators.Passing)
+
         Attributes
         ----------
             private: 
-                left_team_passing_accuracy : float
-                    The accuracy percentage of the left team
-                right_team_passing_accuracy : float
-                    The accuracy percentage of the right team   
-                left_team_total_passes : float
-                    The total number of passes of the left team 
-                right_team_passing_accuracy : float
-                    The total number of passes of the right team 
+                passing_accuracy: dict[str, dict[str, Any]]
+                    Contains passing stats of the teams
 
         Methods
         -------
@@ -24,12 +20,12 @@ class PassingAccuracy(AbstractAnalysis):
                     Calculates all wrong and correct passes occurrences
             public: 
                 results() -> (float, float)
-                    returns the teams passing accuracy and total passes 
+                    Returns the teams passing accuracy and total passes 
                 describe() -> none  
                     Shows the teams respective passing accuracy and total passes
+                serialize() -> dict[str, dict[str, Any]]
+                    Returns an object containing computed stats
     """
-
-
     def __init__(self, data_frame, category, passing_evaluator: Passing):
         self.__passing_accuracy = {}
         self.__category = category
@@ -47,6 +43,9 @@ class PassingAccuracy(AbstractAnalysis):
         return self.__category
 
     def _analyze(self):
+        """
+        Performs match passing accuracy analysis.
+        """
         self.__passing_evaluator.run_passing_evaluation()
         self.__passing_accuracy = {
             'left_team': {
@@ -61,15 +60,30 @@ class PassingAccuracy(AbstractAnalysis):
             }
         }
     
-    def results(self):
+    def results(self) -> tuple[float, float, int, int]:
+        """
+        Returns match passing accuracy stats.
+            
+            Returns:
+                tuple (float, float, int, int): left team passing accuracy, right team passing accuracy left team total passes, right team total passes.
+        """
         return (self.__passing_accuracy['left_team']['accuracy'], self.__passing_accuracy['right_team']['accuracy'], 
                 self.__passing_accuracy['left_team']['total_passes'], self.__passing_accuracy['right_team']['total_passes'])
 
-    def describe(self):
+    def describe(self):        
+        """
+        Shows a teams' passing stats.
+        """
         name_l = self.__current_game_log.loc[1, str(self.category.TEAM_LEFT)]
         name_r = self.__current_game_log.loc[1, str(self.category.TEAM_RIGHT)]
         print(f'{name_l}: acurácia = {self.__passing_accuracy["left_team"]["accuracy"]:.4f}    passes totais = {self.__passing_accuracy["left_team"]["total_passes"]}\n' 
                 f'{name_r}: acurácia = {self.__passing_accuracy["right_team"]["accuracy"]:.4f}    passes totais = {self.__passing_accuracy["right_team"]["total_passes"]}')
 
     def serialize(self):
+        """
+        Returns computed passing accuracy stats for both teams.
+
+                Returns:
+                        passing_accuracy (dict[str, dict[str, Any]]): Overall teams' passing stats.
+        """
         return self.__passing_accuracy
