8d851ac5920473c0635b12fba1ebf34d6556c692
José Ronald <jronaldsilva11@gmail.com>
2022-07-06 20:14:55 -0300

    Feat (FindGoals): now it's possible to get goals moments from just one team if needed

--- socceranalyzer/common/analysis/find_goals.py
+++ socceranalyzer/common/analysis/find_goals.py
@@ -20,6 +20,10 @@ class FindGoals:
                     number of cycles to be analyzed before the occurrence of each goal
                 goal_moments: [int]
                     a list containing dataframe's indexes of moments where goals happened
+                left_team_goals: [int]
+                    a list containing dataframe's indexes of moments where left team goals happened
+                right_team_goals: [int]
+                    a list containing dataframe's indexes of moments where right team goals happened
                 
         Methods
         -------
@@ -35,6 +39,8 @@ class FindGoals:
         self.__dataframe = dataframe
         self.__category = category
         self.__goal_moments = []
+        self.__left_team_goals = []
+        self.__right_team_goals = []
 
         self._analyze()
 
@@ -50,9 +56,25 @@ class FindGoals:
                 # Goal happened
                 self.__goal_moments.append(index)
 
-    def results(self) -> list:
+                if (row[str(self.__category.PLAYMODE)] == str(self.__category.GOAL_SCORED_L)):
+                    self.__left_team_goals.append(index)
+
+                else:
+                    self.__right_team_goals.append(index)
+
+    def results(self, team: str = None) -> list:
         """
+            Parameters:
+                    goal_number (optional) ("left" | "right"): Team's goals to be returned. By default it returns results from both.
+            
             Returns:
                     [int]: dataframe's indexes of moments where goals happened
         """
-        return self.__goal_moments
+        if (team == None):
+            return self.__goal_moments
+        
+        elif (team == "left"):
+            return self.__left_team_goals
+        
+        elif (team == "right"):
+            return self.__right_team_goals
