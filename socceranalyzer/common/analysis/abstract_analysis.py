f658c9a727f61bc3decae08d76649332f9de7ff9
Tiago Henrique <tiagohrpg@gmail.com>
2022-08-04 20:05:28 -0300

    (abstract_analysis): add attributes to init

--- socceranalyzer/common/analysis/abstract_analysis.py
+++ socceranalyzer/common/analysis/abstract_analysis.py
@@ -1,6 +1,16 @@
 from abc import ABCMeta, abstractmethod
 
 class AbstractAnalysis(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, match):
+        self._dataframe = match.dataframe
+        self._category = match.category
+        self._match = match
+
+    @property
+    @abstractmethod
+    def category(self):
+        return self.__category
 
     @abstractmethod
     def _analyze(self):
