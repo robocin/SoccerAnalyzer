2c890faf4002b68c28eb7bba0490bc6beaff6035
jefersonseverino <jsa2@cin.ufpe.br>
2023-01-26 10:09:57 -0300

    fix: fixing distance analysis

--- socceranalyzer/common/analysis/dist_from_corner_mark.py
+++ socceranalyzer/common/analysis/dist_from_corner_mark.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from socceranalyzer.common.analysis.abstract_analysis import AbstractAnalysis
 from socceranalyzer.common.geometric.point import Point
-from socceranalyzer.common.geometric.rectangle import Rectangle
-from socceranalyzer.common.operations.measures import distance_sqrd
+from socceranalyzer.common.operations.measures import distance
 
 class DistFromCornerMark(AbstractAnalysis):
 
@@ -15,8 +14,6 @@ class DistFromCornerMark(AbstractAnalysis):
         self.__category = category
         self.__average_distance = 0.0
         self.__players_inside_area = 0.0
-        self.__upper_corner_area = Rectangle( Point(-52, -34), Point(-36, -16) )
-        self.__down_corner_area = Rectangle( Point(-52, 34), Point(-36, 16) )
 
     @property
     def category(self):
@@ -27,34 +24,27 @@ class DistFromCornerMark(AbstractAnalysis):
         return self.__dataframe
     
     def _analyze(self):
-        corner_dataframe = pd.DataFrame()
         last_playmode = self.__dataframe.iloc[0]['playmode']
-        last_row = self.__dataframe.iloc[0]
 
         for cycle, row in self.__dataframe.iterrows():
             if row['playmode'] != last_playmode and last_playmode == 'corner_kick_l':
-                pd.concat([corner_dataframe, last_row])
-            last_playmode = row['playmode']
-            last_row = row
+                ball_pos = Point(row['ball_x'], row['ball_y'])
 
-        for cycle, row in corner_dataframe.iterrows():
-            ball_pos = Point(row['ball_x'], row['ball_y'])
-
-            for i in range(1, 12):
+                for i in range(1, 12):
                     player = 'player_l{}'.format(i)                
-                    agent_pos = Point(corner_dataframe.loc[cycle, f'{player}_x'], corner_dataframe.loc[cycle, f'{player}_y'])
-
-                    if( (row['ball_y'] < 0 and self.__upper_corner_area.is_inside(agent_pos)) or 
-                        (row['ball_y'] > 0 and self.__down_corner_area.is_inside(agent_pos)) ):
-                        self.__average_distance += distance_sqrd(agent_pos, ball_pos)
+                    agent_pos = Point(row[f'{player}_x'], row[f'{player}_y'])
+                    
+                    if(distance(ball_pos, agent_pos) < 25):
+                        self.__average_distance += distance(agent_pos, ball_pos)
                         self.__players_inside_area += 1
+
+            last_playmode = row['playmode']
 
         if self.__players_inside_area != 0:        
             self.__average_distance /= self.__players_inside_area
 
     def describe(self):
         print(f'Average distance of players from corner mark is {self.__average_distance}')
-        return self.__average_distance
 
     def results(self):
         return self.__average_distance
