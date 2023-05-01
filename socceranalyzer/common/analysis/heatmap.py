990bcb724f45715816f84f97ed33c68af08c5aa9
José Ronald <jronaldsilva11@gmail.com>
2023-05-01 19:15:45 -0300

    fix: filtering dataframe before calculating heatmap positions

--- socceranalyzer/common/analysis/heatmap.py
+++ socceranalyzer/common/analysis/heatmap.py
@@ -40,25 +40,28 @@ class Heatmap(AbstractAnalysis):
         return self.__category
 
     def _analyze(self):
+        # Filtering to playmode where the game is running
+        self.__dataframe = self.__dataframe[self.__dataframe[str(self.__category.PLAYMODE)] == self.__category.RUNNING_GAME]
+
         left_players_column = Mediator.players_left_position(self.category, gkeeper=False)
         right_players_column = Mediator.players_right_position(self.category, gkeeper=False)
         
         for i in range(len(left_players_column.items)):
-            self.left_players_x = self.left_players_x + self.dataframe[left_players_column.items[i].x].values.tolist()
-            self.left_players_y = self.left_players_y + self.dataframe[left_players_column.items[i].y].values.tolist()
+            self.left_players_x = self.left_players_x + self.__dataframe[left_players_column.items[i].x].values.tolist()
+            self.left_players_y = self.left_players_y + self.__dataframe[left_players_column.items[i].y].values.tolist()
 
         for i in range(len(right_players_column.items)):
-            self.right_players_x = self.right_players_x + self.dataframe[right_players_column.items[i].x].values.tolist()
-            self.right_players_y = self.right_players_y + self.dataframe[right_players_column.items[i].y].values.tolist()
+            self.right_players_x = self.right_players_x + self.__dataframe[right_players_column.items[i].x].values.tolist()
+            self.right_players_y = self.right_players_y + self.__dataframe[right_players_column.items[i].y].values.tolist()
 
     def plot_left(self):
-        hist2d_y = plt.hist2d(np.array(self.left_players_x), np.array(self.left_players_y), bins=30, cmap='Greens')
-        plt.plot(hist2d_y)
+        hist2d_left = plt.hist2d(np.array(self.left_players_x), np.array(self.left_players_y), bins=30, cmap='Greens')
+        plt.plot(hist2d_left)
         plt.show()
 
     def plot_right(self):
-        hist2d_y = plt.hist2d(np.array(self.right_players_x), np.array(self.right_players_y), bins=30, cmap='Reds')
-        plt.plot(hist2d_y)
+        hist2d_right = plt.hist2d(np.array(self.right_players_x), np.array(self.right_players_y), bins=30, cmap='Reds')
+        plt.plot(hist2d_right)
         plt.show()
 
     def describe(self):
